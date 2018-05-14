adc55bab313ae2d3adfd1c3cb86bac9d3e310902
antony-jr <antonyjr@protonmail.com>
2018-05-14 20:42:49 +0530

    Reduce size test

--- scripts/copy_libs.sh
+++ scripts/copy_libs.sh
@@ -8,23 +8,7 @@
 
 # Required libraries.
 cp /usr/share/tcltk/* -p -r AppDir/usr/lib/
-cp /usr/bin/tclsh8.5 -p -r AppDir/usr/bin/
-cp /usr/include/tcl8.5 -p -r AppDir/usr/include/
-cp /usr/lib/tcl8.5 -p -r AppDir/usr/lib/
-cp /usr/lib/tk8.5 -p -r AppDir/usr/lib/
 cp /usr/lib/tcltk -p -r AppDir/usr/lib/
-mkdir AppDir/usr/lib/x86_64-linux-gnu
-cp /usr/lib/x86_64-linux-gnu/libtcl8.5.a -p AppDir/usr/lib/x86_64-linux-gnu/
-cp /usr/lib/x86_64-linux-gnu/libtclstub8.5.a -p AppDir/usr/lib/x86_64-linux-gnu/
-cp /usr/lib/x86_64-linux-gnu/tcl8.5 -p -r AppDir/usr/lib/x86_64-linux-gnu/
-cp /usr/lib/x86_64-linux-gnu/tk8.5 -p -r AppDir/usr/lib/x86_64-linux-gnu/
-cp /usr/lib/x86_64-linux-gnu/libtk8.5.a -p AppDir/usr/lib/x86_64-linux-gnu/
-cp /usr/lib/x86_64-linux-gnu/libtkstub8.5.a -p AppDir/usr/lib/x86_64-linux-gnu/
-cp /usr/lib/x86_64-linux-gnu/libtk8.5.so -p AppDir/usr/lib/x86_64-linux-gnu/
-cp /usr/lib/x86_64-linux-gnu/libtk8.5.so.0 -p AppDir/usr/lib/x86_64-linux-gnu/
-cp /usr/share/lintian -p -r AppDir/usr/share/
-mkdir AppDir/usr/share/aclocal
-cp /usr/share/aclocal/tcl8.5.m4 -p AppDir/usr/share/aclocal/
 cp /usr/lib/libitcl3.4.so.1 -p AppDir/usr/lib/
 cp /usr/lib/libitcl3.4.so -p AppDir/usr/lib/
 cp /usr/lib/libitclstub3.4.a -p AppDir/usr/lib/
