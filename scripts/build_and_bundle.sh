c0abc08194b3cc5734efe463337e9e09487d5eee
antony-jr <antonyjr@protonmail.com>
2019-06-05 21:07:15 +0530

    do not build unwanted binary

--- scripts/build_and_bundle.sh
+++ scripts/build_and_bundle.sh
@@ -9,7 +9,18 @@ install_dir=$(pwd)/AppDir
 cd insight/
 autoconf
 chmod +x configure
-./configure --prefix=/usr/
+./configure	--prefix=/usr/.						\
+		--disable-binutils					\
+		--disable-elfcpp					\
+		--disable-gas						\
+		--disable-gold						\
+		--disable-gprof						\
+		--disable-ld						\
+		--disable-rpath						\
+		--disable-zlib						\
+		--without-libunwind					\
+		--without-isl
+
 make -j$(nproc)
-make DESTDIR=$install_dir install -j4
+make DESTDIR=$install_dir install -j$(nproc)
 cd ..
