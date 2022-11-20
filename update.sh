b3f15e467095791d2a702045600ac5e9b84f925c
antony-jr <antonyjr@protonmail.com>
2022-11-20 20:51:26 +0530

    [RELEASE 5afb39f8fd70c3517a48af50a72c9ef9ed70d2a8] Insight 5afb39f
    Changelog:
    
       * Upgrade submodule, sync patches. (upstream)
       * Fix requirement of absolute paths.
    
    Signed-off-by: Divya Antony J.R <antonyjr@protonmail.com>

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-
-echo -e "\e[1;34mInsight (git-commit 93ab4f7)\e[0m, Tcl/Tk GUI for GDB Debugger."
+COMMIT="5afb39f"
+echo -e "\e[1;34mInsight (git-commit $COMMIT)\e[0m, Tcl/Tk GUI for GDB Debugger."
 echo -e "Copyright (C) Redhat, GNU General Public License v2."
 echo -e "AppImage Packaged by Antony J.R <antonyjr@pm.me>."
 echo ""
@@ -27,7 +27,7 @@ $(pwd)/.appimageupdater -t -D insight
 if [ -f *.AppImage ]; then
    mv *.AppImage insight
 else
-   echo -e "\e[42mYou already have latest Insight Stable (git-commit 93ab4f7) (Target CPU: x86_64).\e[0m"
+   echo -e "\e[42mYou already have latest Insight Stable (git-commit $COMMIT) (Target CPU: x86_64).\e[0m"
    exit
 fi
 
@@ -42,7 +42,7 @@ rm -rf squashfs-root
 
 mv squashfs-root/insight-icon.png .icons/insight.png
 
-sed -i 's+Name=Insight+Name=Insight GDB Debugger (git-93ab4f7)+g' squashfs-root/Insight.desktop
+sed -i 's+Name=Insight+Name=Insight GDB Debugger (git-'$COMMIT')+g' squashfs-root/Insight.desktop
 sed -i 's+Exec=insight-wrapper+Exec='$HOME'\/Insight\/insight+g' squashfs-root/Insight.desktop
 sed -i 's+Icon=insight-icon+Icon='$HOME'\/Insight\/.icons\/insight.png+g' squashfs-root/Insight.desktop
 
@@ -53,4 +53,4 @@ rm -rf squashfs-root
 
 echo "Updated ~/.bashrc, Restart your Console/Terminal."
 echo -e "\e[1;34mStart Insight from your Application Menu.\e[0m"
-echo -e "\e[42mUpdated to Insight Stable (git-commit 93ab4f7) (Target CPU: x86_64).\e[0m"
+echo -e "\e[42mUpdated to Insight Stable (git-commit $COMMIT) (Target CPU: x86_64).\e[0m"
